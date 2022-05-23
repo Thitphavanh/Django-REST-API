f579cac2408028e3d56993b3ab829901ba5631db
Hery <thitphavanh.psv@gmail.com>
2022-05-23 16:35:12 +0700

    update

--- mydjangoapi/myapp/views.py
+++ mydjangoapi/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+data = [{'message': 'hello world'}, {'message': 'hello world'}]
+
+
+def Home(request):
+    return JsonResponse(data=data, safe=False, json_dumps_params={'ensure_ascii': False})
--- mydjangoapi/mydjangoapi/settings.py
+++ mydjangoapi/mydjangoapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+^n-y3l=@r^f$r7bk-!k(xzm=2tu=n)ul-svb!1=1qpsbxr(yq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
