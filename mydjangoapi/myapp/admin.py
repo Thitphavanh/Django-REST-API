dad0dd5262792590058cea17f54e8d0e29c00908
Hery <thitphavanh.psv@gmail.com>
2022-05-28 15:14:34 +0700

    update

--- mydjangoapi/myapp/admin.py
+++ mydjangoapi/myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+admin.site.site_header = 'Phenomenal Shop'
+admin.site.index_title = 'Main Admin'
+admin.site.site_title = 'Phenomenal Shop Backend'
+
+class ProductsAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name', 'price', 'detail','size', 'quantity','imagePath']
+	list_editable = ['name', 'price', 'detail','size', 'quantity', 'imagePath']
+
+admin.site.register(Product,ProductsAdmin)
+
--- mydjangoapi/myapp/models.py
+++ mydjangoapi/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.CharField(max_length=100)
+	size = models.CharField(max_length=50, blank=True, null=True)
+	detail = models.TextField(null=True, blank=True)
+	quantity = models.IntegerField(default=1)
+	imagePath = models.ImageField(upload_to="products", null=True, blank=True)
+
+	def __str__(self):
+		return self.name
+
+
+ 
