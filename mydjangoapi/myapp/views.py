1cf73e8b8fd6381f4646e29e203a5fa37dff26ee
Hery <thitphavanh.psv@gmail.com>
2022-05-28 12:32:45 +0700

    update

--- mydjangoapi/myapp/views.py
+++ mydjangoapi/myapp/views.py
@@ -1,7 +1,85 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-data = [{'message': 'hello world'}, {'message': 'hello world'}]
+data = [
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Liverpool FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/Liverpool.jpg',
+        'size': 'M',
+        'stock': '100',
+        'price': '250000'
+    },
+
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Liverpool FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/Liverpool.jpg',
+        'size': 'L',
+        'stock': '150',
+        'price': '250000'
+    },
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Liverpool FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/Liverpool.jpg',
+        'size': 'XL',
+        'stock': '100',
+        'price': '250000'
+    },
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Liverpool FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/Liverpool.jpg',
+        'size': 'XXL',
+        'stock': '80',
+        'price': '250000'
+    },
+
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Manchester United FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/man-united.jpg',
+        'size': 'M',
+        'stock': '100',
+        'price': '290000'
+    },
+
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Manchester United FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/man-united.jpg',
+        'size': 'L',
+        'stock': '100',
+        'price': '290000'
+    },
+
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Manchester United FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/man-united.jpg',
+        'size': 'XL',
+        'stock': '100',
+        'price': '290000'
+    },
+
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Manchester United FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/man-united.jpg',
+        'size': 'XL',
+        'stock': '100',
+        'price': '290000'
+    },
+    {
+        'detail': 'New jersey 2022/2023',
+        'name': 'Manchester United FC',
+        'pathImages': 'https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/man-united.jpg',
+        'size': 'XXL',
+        'stock': '100',
+        'price': '290000'
+    }
+]
 
 
 def Home(request):
